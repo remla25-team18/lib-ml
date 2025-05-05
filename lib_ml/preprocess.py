48758359775e15016a89d1e42fb2b1a23ea5ac5d
Yahor Dziomin <y.y.dziomin@student.tudelft.nl>
2025-05-05 09:59:30 +0200

    Make the lib-ml more versatile

--- lib_ml/preprocess.py
+++ lib_ml/preprocess.py
@@ -10,28 +10,29 @@ from nltk.stem.porter import PorterStemmer
 # Download stopwords from nltk
 nltk.download("stopwords")
 
-
-def preprocess(file : str):
-    """Preprocesses the given dataset.
-
+def preprocess_text(reviews):
+    """
+    Preprocesses a list of review texts.
+    
     Parameters:
-        - file: (str), path to the dataset file
+        - reviews: (List[str] or pd.DataFrame), raw reviews or a DataFrame with a 'Review' column.
 
     Returns:
-        - tuple (Preprocessed reviews, respective labels)    
+        - List of cleaned, stemmed review texts.
     """
-    dataset = pd.read_csv(file, delimiter="\t", quoting=3)
-
+    if isinstance(reviews, pd.DataFrame):
+        reviews = reviews["Review"].tolist()
+    
     ps = PorterStemmer()
     all_stopwords = stopwords.words("english")
     if "not" in all_stopwords:
         all_stopwords.remove("not")
 
     corpus = []
-    for review in dataset["Review"]:
+    for review in reviews:
         review = re.sub("[^a-zA-Z]", " ", review)
         review = review.lower().split()
         review = [ps.stem(word) for word in review if word not in set(all_stopwords)]
         corpus.append(" ".join(review))
-
-    return corpus, dataset.iloc[:, -1].values
+    
+    return corpus
